43f6ee2f13bf1968d951976fa222aa6a4c5f681f
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-04-18 20:12:33 +0600

    new commit

---
+++ Module-3/Nested_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    for(int i = 0; i < 12; i++){
+        for(int j =1; j <= 8; j++){
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
