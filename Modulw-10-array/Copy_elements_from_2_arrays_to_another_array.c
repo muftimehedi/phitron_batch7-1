e901ba658e4fde2629cdacf5db0f3cf772880877
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-04-24 01:06:58 +0600

    new commit

--- Modulw-10-array/Copy_elements_from_2_arrays_to_another_array.c
+++ Modulw-10-array/Copy_elements_from_2_arrays_to_another_array.c
@@ -8,7 +8,7 @@ int main(){
     }
 
 
-    
+
 
     int m;
     scanf("%d", &m);
@@ -16,4 +16,19 @@ int main(){
     for(int i = 0; i < m; i++){
         scanf("%d", &b[i]);
     }
+
+    int c [n + m];
+    for(int i = 0; i<n; i++){
+        c[i] = a[i];
+    }
+
+    for(int i =0; i< m; i++){
+        c[i+n] = b[i];
+    }
+
+    for(int i =0; i<n + m;i++){
+        printf("%d ", c[i]);
+    }
+
+    return 0;
 }
