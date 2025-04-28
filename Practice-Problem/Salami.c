74c6f4d88b933f8a9412428b603fc77b1646b2fa
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-04-28 12:44:50 +0600

    new commit

---
+++ Practice-Problem/Salami.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int X, Y, Z;
+    scanf("%d %d %d", &X, &Y, &Z);
+    
+    int total = X + Y + Z;
+    
+    printf("%d", total );
+    return 0;
+}
